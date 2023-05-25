81d1ae3e2cb6656ceddbdbe1b89c7681a4c83c3e
kcrosas <kencrosas@gmail.com>
2023-05-25 00:22:54 -0400

    Add additional links to activate Chrome extension

--- action.js
+++ action.js
@@ -2,17 +2,17 @@ const url = window.location.href;
 console.log(url, "Now you can focus...");
 function myFunc() {
   console.log('hello')
-  if (location.href === "https://www.linkedin.com/feed/"){
+  if (location.href === "https://www.linkedin.com/feed/" || location.href === "https://www.linkedin.com/feed/?trk=homepage-basic_sign-in-submit"){
   const items = document.querySelector(".scaffold-layout__main")
   const toClean = items.children[2]
   toClean.innerHTML = ''
   }
  console.log(location.href)
 }
-myFunc()
+
 let previousUrl = '';
 const observer = new MutationObserver(function(mutations) {
-  if (location.href === "https://www.linkedin.com/feed/"){
+  if (location.href === "https://www.linkedin.com/feed/" || location.href === "https://www.linkedin.com/feed/?trk=homepage-basic_sign-in-submit"){
     myFunc()
   }
 });
